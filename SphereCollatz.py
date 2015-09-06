e35e8699221f9622579196f7525447fae736f478
Mathew Kurian <bluejamesbond@gmail.com>
2015-09-06 00:37:01 -0500

    Adding SphereCollatz

---
+++ SphereCollatz.py
@@ -0,0 +1,134 @@
+#!/usr/bin/env python3
+
+import sys
+
+
+# ---------------------------
+# projects/collatz/Collatz.py
+# Copyright (C) 2015
+# Glenn P. Downing
+# ---------------------------
+
+# ------------
+# collatz_read
+# ------------
+
+def collatz_read(s):
+    """
+    read two ints
+    s a string
+    return a list of two ints, representing the beginning
+    and end of a range, [i, j]
+    """
+    a = s.split()
+    return [int(a[0]), int(a[1])]
+
+
+# ------------
+# collatz_eval
+# ------------
+
+def collatz_eval(i, j):
+    """
+    i the beginning of the range, inclusive
+    j the end       of the range, inclusive
+    return the max cycle length of the range [i, j]
+    """
+
+    # TODO what if i == 0 or j == 0? what is input range?
+
+    max_cycles = 1
+
+    if j < i:
+        temp = i  # swap j and i
+        i = j
+        j = temp
+
+    for curr in range(i, j + 1):
+        cycles = 1
+        while curr > 1:
+            if curr % 2 == 0:
+                curr /= 2
+            else:
+                curr = 3 * curr + 1
+            cycles += 1
+
+            if cycles > max_cycles:
+                max_cycles = cycles
+
+    return max_cycles
+
+
+# -------------
+# collatz_print
+# -------------
+
+def collatz_print(w, i, j, v):
+    """
+    print three ints
+    w a writer
+    i the beginning of the range, inclusive
+    j the end       of the range, inclusive
+    v the max cycle length
+    """
+    w.write(str(i) + " " + str(j) + " " + str(v) + "\n")
+
+
+# -------------
+# collatz_solve
+# -------------
+
+def collatz_solve(r, w):
+    """
+    r a reader
+    w a writer
+    """
+    for s in r:
+        i, j = collatz_read(s)
+        v = collatz_eval(i, j)
+        collatz_print(w, i, j, v)
+
+
+# !/usr/bin/env python3
+
+# ------------------------------
+# projects/collatz/RunCollatz.py
+# Copyright (C) 2015
+# Glenn P. Downing
+# ------------------------------
+
+# -------
+# imports
+# -------
+
+# ----
+# main
+# ----
+
+if __name__ == "__main__":
+    collatz_solve(sys.stdin, sys.stdout)
+
+"""
+% cat RunCollatz.in
+1 10
+100 200
+201 210
+900 1000
+
+
+
+% RunCollatz.py < RunCollatz.in > RunCollatz.out
+
+
+
+% cat RunCollatz.out
+1 10 1
+100 200 1
+201 210 1
+900 1000 1
+
+
+
+% pydoc3 -w Collatz
+# That creates the file Collatz.html
+"""
